4fb180108c3fb9adccef8f4ff289b27528f3e7aa
Spencer Marlen-Starr <spencermstarr@hotmail.com>
2023-03-07 15:11:52 +0100

    Add files via upload

--- Team AgniFuego's SQL script.sql
+++ Team AgniFuego's SQL script.sql
@@ -108,13 +108,37 @@ FROM dash_air_scheds
 INNER JOIN dash_airports ON dash_air_scheds.Dep_Airport_ID = dash_airports.Dep_Airport_ID;
 -- QUERIES & COMMANDS THAT WORK: END
 
+
+-- Runway table query, version 1
 SELECT dasda.Dep_Airport_ID, dasda.Schedules, dasda.Facility_Type, dasda.Wind_Indicator, dash_runs.Runway_ID, dash_runs.Length, dash_runs.Width, dash_runs.Surface_Type_Condition, dash_runs.Edge_Light_Intensity, dash_runs.WBC_Single, dash_runs.WBC_Dual, dash_runs.WBC_Two_Dual, dash_runs.WBC_Tandem_Two_Dual, dash_runs.Base_Marking_Condition 
 FROM dash_air_scheds_on_dash_airports AS dasda
 INNER JOIN dash_runs ON dasda.Dep_Airport_ID = dash_runs.Dep_Airport_ID;
 
+-- Runway table query, version 2
 SELECT dasdr.Dep_Airport_ID, dasdr.Schedules, dash_airports.Facility_Type, dash_airports.Wind_Indicator, dasdr.Runway_ID, dasdr.Length, dasdr.Width, dasdr.Surface_Type_Condition, dasdr.Edge_Light_Intensity, dasdr.WBC_Single, dasdr.WBC_Dual, dasdr.WBC_Two_Dual, dasdr.WBC_Tandem_Two_Dual, dasdr.Base_Marking_Condition 
 FROM dash_air_scheds_on_dash_runs AS dasdr
 INNER JOIN dash_airports ON dasdr.Dep_Airport_ID = dash_airports.Dep_Airport_ID;
+
+
+
+SELECT * FROM airport_schedules;
+SELECT * FROM airports;
+-- inner join of airport_schedules on airports
+SELECT airport_schedules.Dep_Airport_ID, airport_schedules.Schedules, airports.Facility_Type, airports.Wind_Indicator
+FROM airport_schedules
+INNER JOIN airports ON airport_schedules.Dep_Airport_ID = airports.Dep_Airport_ID;
+
+-- Runway table query, version 3
+SELECT sch.Dep_Airport_ID, sch.Schedules, sch.Facility_Type, sch.Wind_Indicator, run.Runway_ID, run.Length, run.Width, run.Surface_Type_Condition, run.Edge_Light_Intensity, run.WBC_Single, run.WBC_Dual, run.WBC_Two_Dual, run.WBC_Tandem_Two_Dual, run.Base_Marking_Condition
+FROM airport_schedules_on_airports AS sch
+INNER JOIN runway AS run ON sch.Dep_Airport_ID = run.Dep_Airport_ID
+WHERE sch.Dep_Airport_ID IN ('09J','14A','18A','1V6','22S','2I0','2R4','32S','38S','46U','48A','4S2','65S','6A8','6K4','6K8','6S0','6S3','6S5','74S','77S','7AK','7S0','88AZ','8S0','8S1','9A3','AAO','ABI','ABQ','ABR','ACT','ACV','ACY','ADH','ADK','ADM','ADQ','ADS','AEG','AFW','AGC','AGS','AHN','AIK','AIT','AIZ','AK15','ALM','ALS','ALW','AMA','ANC','ANE','APA','APC','APF','APV','AQO','AQW','ASE','AST','ATW','AUM','AUN','AUS','AVL','AVQ','AWO','AYPY','AZE','BAK','BAM','BBG','BCT','BDL','BDN','BDR','BEA','BET','BFF','BFI','BFL','BFM','BGQ','BGR','BGSF','BHK','BHM','BIG','BIL','BIS','BJC','BJI','BKE','BKS','BKV','BLI','BMT','BNA','BNO','BOI','BOW','BPG','BRD','BRG','BRW','BTF','BTM','BUR','BVO','BVS','BVU','BWG','BWI','BZN','C35','C83','CAT4','CBS8','CDB','CDC','CDS','CDV','CEC','CEW','CEZ','CGI','CHA','CHS','CIC','CLL','CLM','CLW','CMA','CNI','CNO','CNY','COD','COE','COQ','COS','CPR','CPS','CRG','CRP','CRQ','CTB','CTJ','CUB','CVN','CVO','CXO','CXP','CXY','CYCD','CYCQ','CYLW','CYQL','CYQT','CYS','CYSU','CYVR','CYXE','CYXX','CYXY','CYYC','CYYF','CYYG','CYYJ','CYYR','CYZU','CZBB','DAG','DAL','DBQ','DEW','DFI','DHN','DIK','DKX','DLG','DLH','DLN','DLS','DMA','DMN','DNL','DPA','DPG','DRO','DTO','DUT','DVT','DZB','E25','EAR','EAT','ECP','EDF','EDN','EED','EEN','EET','EFD','EGE','EGTC','EKO','EKQ','ELO','ELP','ELY','EMT','ENA','ENN','ENV','ENW','EQY','EUG','EVB','EVV','EVW','EWK','EYW','F05','FAI','FAR','FAT','FBK','FCM','FDK','FET','FFC','FFT','FFZ','FHR','FHU','FLG','FLL','FMM','FMN','FNL','FOT','FPR','FSD','FSM','FTW','FUL','FXE','FYU','FYV','GAL','GCC','GCD','GCK','GEG','GEU','GEY','GGG','GIC','GJT','GKN','GLR','GLW','GMU','GNB','GNC','GON','GPI','GPZ','GRB','GSO','GST','GTF','GTR','GTU','GVT','GYH','GYR','GYY','HAF','HDN','HEF','HEZ','HFY','HIB','HIF','HII','HIO','HLA','HLN','HMP','HMT','HND','HNL','HNS','HOM','HON','HOT','HOU','HPB','HRF','HSA','HSP','HUM','HUT','HXD','HZD','I18','IAB','IAD','IAH','ICT','IDA','IFP','IGM','IND','INL','INS','INW','IOB','IRK','ISM','ITH','ITO','IWA','JAC','JAN','JAX','JBR','JER','JFZ','JMS','JNU','JQF','JTC','JWN','JYO','JZI','JZP','KTN','KVC','L35','LAF','LAL','LAN','LAR','LAS','LAX','LBB','LBF','LBL','LCQ','LFT','LGB','LGD','LHM','LIT','LKV','LLJ','LMT','LND','LNK','LOL','LOZ','LSE','LTY','LUD','LUK','LVK','LVS','LWA','LWS','LWT','LZU','M02','M17','M25','M29','M54','MAF','MAN','MBY','MCC','MCE','MCG','MCO','MEI','MER','MEV','MFR','MHR','MIA','MIT','MKC','MKJS','MKL','MKT','MKY','MLB','MLI','MLS','MLU','MLY','MMAA','MMCL','MMHO','MMLP','MMPR','MMSD','MMTM','MMTP','MMU','MMV','MMVR','MMZH','MNV','MOD','MPTO','MQI','MRB','MRF','MRI','MRY','MSO','MSP','MTJ','MVI','MWC','MWH','MWL','MYAF','MYAM','MYL','MZBZ','MZJ','NEW','NLC','NQA','NSTU','NUL','NUQ','NYL','O22','O32','OAK','OCF','ODO','OGD','OGG','OKC','OLM','OLS','OMA','OME','ONO','ONP','ONT','OPN','ORL','OSC','OTH','OWD','OXR','OZW','P13','P20','P52','PAE','PAH','PAN','PAQ','PBV','PDK','PDT','PDX','PEO','PGA','PGUM','PHL','PHT','PHX','PIA','PIH','PIR','PKMJ','PLN','PMD','PNC','PNE','POU','PRB','PRC','PSC','PSG','PSP','PTV','PUB','PUC','PUW','PVF','PVU','RAP','RBG','RBL','RCE','RDD','RDM','REO','RFD','RIC','RID','RIF','RIL','RIV','RJAA','RKD','RKS','RMN','RNO','RNT','ROA','ROW','RRT','RST','RSW','RTS','RVF','RYN','RYY','RZR','S21','S25','S33','S39','S52','S97','SAA','SAC','SAD','SAF','SAN','SAT','SAV','SAW','SBA','SBD','SBP','SBS','SC3','SCK','SDL','SDP','SDY','SEZ','SFF','SFM','SFO','SGF','SGS','SGT','SGU','SHR','SHV','SIK','SIT','SIY','SJC','SJS','SJT','SKX','SLC','SLE','SLI','SMF','SMN','SMX','SNA','SNP','SNS','SNY','SOP','SPB','SPF','SPI','SPS','SRQ','SSF','STP','STS','SUA','SUE','SUN','SVC','SVH','SWI','SXQ','SZT','T31','T82','TAL','TCM','TIW','TIX','TKX','TLH','TME','TMK','TOA','TOL','TOP','TRI','TRK','TRL','TRM','TUL','TUS','TVC','TVL','TVR','TWF','TWM','TXK','TYS','U10','U42','UAO','UBW','UGN','UHPP','UKI','UNI','UNV','UOX','UVA','VCT','VDI','VDZ','VEL','VGT','VIH','VKS','VNC','VNY','VQQ','VUO','WDR','WHP','WJF','WRG','WVI','WWD','WYS','X21','X60','XBP','XWA','Y51','YAK','YKM','YKN');
+
+
+
+
+
+
 
 
 
